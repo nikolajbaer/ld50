500f8878f39a56b2d066e8449d60f8762c4e6ccd
Nikolaj Baer <nikolaj.baer@gmail.com>
2022-04-02 08:40:00 -0700

    basic flow and rise mechanics

--- Snowball.gd
+++ Snowball.gd
@@ -5,26 +5,24 @@ signal grabbed
 var water_y
 var bob
 var float_velocity
-var floating
 var BOB_X = 3 # controls intensity of bob, adjusted to scale
 var BOB_FQ = 0.4 # higher makes bob slower
-var grabbed
 var camera
 # https://github.com/christinoleo/godot-plugin-DragDrop3D/blob/master/addons/DragDrop3D/DragDropController.gd
-var ray_length = 100
-var last_mouse
-var can_grab = false
+
 var grab_point
+
+var state
+enum {FREE,FLOATING,GRABBED,STUCK}
 
 func _ready():
 	#BOB_X *= scale.x
-	floating = false
-	grabbed = false
+	state = FREE
 	camera = get_tree().get_root().get_camera()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if grabbed:
+	if state == GRABBED:
 		var mouse = get_viewport().get_mouse_position()
 
 		# https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
@@ -40,10 +38,11 @@ func _process(delta):
 			translation = l0 + t * l
 			if translation.y < water_y:
 				translation.y = water_y
-			
-	elif floating:
+			if translation.z > grab_point.z:
+				translation.z = grab_point.z
+	elif state == FLOATING or state == STUCK:
 		translation += float_velocity * delta
-		if bob < 10: # TODO make this a buoyant force instead..
+		if state == FLOATING and bob < 10: # TODO make this a buoyant force instead..
 			translation.y = -1 * (sin(BOB_X*bob)/BOB_X * BOB_FQ/bob) + water_y
 			bob += delta
 
@@ -53,22 +52,39 @@ func start_float():
 	set_mode(RigidBody.MODE_KINEMATIC)
 	bob = 0.01
 	water_y = translation.y
-	floating = true
-	can_grab = true
-	
+	state = FLOATING
+
+func stick_to_ice(flow_vel):
+	set_mode(RigidBody.MODE_KINEMATIC)
+	float_velocity = flow_vel
+	bob = 100 # Skip the bobbing on the glacier
+	state = STUCK
+
+func release_from_ice():
+	set_mode(RigidBody.MODE_RIGID)
+	state = FREE
+
 func start_grab():
-	grabbed = true
-	floating = false
+	state = GRABBED
 	grab_point = translation
 	set_mode(RigidBody.MODE_KINEMATIC)
 	emit_signal("grabbed")
 
 func release_grab(vel):
-	grabbed = false
+	state = FREE
 	linear_velocity = vel
 	set_mode(RigidBody.MODE_RIGID)
 
+func is_stuck():
+	return state == STUCK
+
+func is_floating():
+	return state == FLOATING
+
+func is_grabbed():
+	return state == GRABBED
+
 func _on_Snowball_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.is_pressed() and floating:
+		if event.is_pressed() and state == FLOATING:
 			start_grab()
--- root.gd
+++ root.gd
@@ -5,10 +5,24 @@ var Rock = preload("res://Snowball.tscn")
 var rate
 var t
 var current_snowball
+var flow_vec
+var flow_vel # current flow velocity
+var release_z
+const GLOBAL_WARMING = 0.01# Rate at which ice flow increases (relative to delta seconds)
+var restored_snow
+var melt_level 
+var start_sea_level
+const MELT_SPEED = 0.1 # effect of iceberg count on sea level rise 
 
 func _ready():
 	rate = 3.0
 	t = 0
+	flow_vel = 0
+	flow_vec = $Glacier/CollisionShape.global_transform.basis.z.normalized()
+	release_z = $Glacier/DropPoint.global_transform.origin.z # plane which when crossed snow falls back off the glacier
+	restored_snow = []
+	melt_level = 0
+	start_sea_level = $Ocean.global_transform.origin.y
 
 func _process(delta):
 	if t > rate:
@@ -20,20 +34,36 @@ func _process(delta):
 		b.rotation.x = rand_range(-1,1)
 		b.rotation.z = rand_range(-1,1)
 		b.connect("grabbed",self,"_snowball_grabbed",[b])
+		b.add_to_group("snowballs")
 		add_child(b)
 		rate *= rand_range(0.95,1.0)
 	else:
 		t += delta
-
-func _on_Area_body_entered(body):
+	flow_vel += delta * GLOBAL_WARMING
+	
+	for snow in get_tree().get_nodes_in_group("snowballs"):
+		if snow.is_stuck() and snow.global_transform.origin.z > release_z:
+			snow.release_from_ice()
+	$Ocean.translation.y = start_sea_level + melt_level * MELT_SPEED
+	
+func _on_Ocean_body_entered(body):
 	if body.get_mode() == RigidBody.MODE_RIGID:
-		body.start_float()	
-
-func _snowball_grabbed(snowball):
-	current_snowball = snowball
+		body.start_float()
+		melt_level += 1
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if not event.is_pressed() and current_snowball:
 			current_snowball.release_grab(Vector3())
 			current_snowball = null
+
+func _snowball_grabbed(snowball):
+	current_snowball = snowball
+
+func _on_Glacier_body_entered(body):
+	if body.get_mode() == RigidBody.MODE_RIGID:
+		body.stick_to_ice(flow_vec * flow_vel)
+		
+func _on_Ocean_body_exited(body):
+	if body.is_grabbed():
+		melt_level -= 1	

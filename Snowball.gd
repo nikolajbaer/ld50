9875985ca6664b412dfbbca34f5147b929581216
Nikolaj Baer <nikolaj.baer@gmail.com>
2022-04-02 00:18:38 -0700

    somewhat throwing, but need to tweak how we move in 3d, add constraints, enforce collisions

--- Snowball.gd
+++ Snowball.gd
@@ -1,21 +1,74 @@
 extends RigidBody
 
+signal grabbed
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var water_y
+var bob
+var float_velocity
+var floating
+var BOB_X = 3 # controls intensity of bob, adjusted to scale
+var BOB_FQ = 0.4 # higher makes bob slower
+var grabbed
+var camera
+# https://github.com/christinoleo/godot-plugin-DragDrop3D/blob/master/addons/DragDrop3D/DragDropController.gd
+var ray_length = 100
+var last_mouse
+var can_grab = false
+var grab_point
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	#BOB_X *= scale.x
+	floating = false
+	grabbed = false
+	camera = get_tree().get_root().get_camera()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if grabbed:
+		var mouse = get_viewport().get_mouse_position()
 
+		# https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
+		var n = Vector3(-1,0,0) # intersect plane 45 deg back towards the glacier
+		var p0 = grab_point
+		var l0 = camera.project_ray_origin(mouse)
+		var l = camera.project_ray_normal(mouse).normalized()
+		var denom = n.dot(l)
+		if denom > 0.0001:
+			var p0l0 = p0 - l0
+			var t = p0l0.dot(n) / denom
+			
+			translation = l0 + t * l
+			if translation.y < water_y:
+				translation.y = water_y
+			
+	elif floating:
+		translation += float_velocity * delta
+		if bob < 10: # TODO make this a buoyant force instead..
+			translation.y = -1 * (sin(BOB_X*bob)/BOB_X * BOB_FQ/bob) + water_y
+			bob += delta
+
+func start_float():
+	float_velocity = linear_velocity * 0.5
+	float_velocity.y = 0
+	set_mode(RigidBody.MODE_KINEMATIC)
+	bob = 0.01
+	water_y = translation.y
+	floating = true
+	can_grab = true
+	
+func start_grab():
+	grabbed = true
+	floating = false
+	grab_point = translation
+	set_mode(RigidBody.MODE_KINEMATIC)
+	emit_signal("grabbed")
+
+func release_grab(vel):
+	grabbed = false
+	linear_velocity = vel
+	set_mode(RigidBody.MODE_RIGID)
 
 func _on_Snowball_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouse:
-		print("Click Snowball")
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if event.is_pressed() and floating:
+			start_grab()
--- root.gd
+++ root.gd
@@ -4,6 +4,7 @@ var Rock = preload("res://Snowball.tscn")
 
 var rate
 var t
+var current_snowball
 
 func _ready():
 	rate = 3.0
@@ -18,11 +19,21 @@ func _process(delta):
 		b.scale *= rand_range(0.5,1.5)
 		b.rotation.x = rand_range(-1,1)
 		b.rotation.z = rand_range(-1,1)
+		b.connect("grabbed",self,"_snowball_grabbed",[b])
 		add_child(b)
 		rate *= rand_range(0.95,1.0)
 	else:
 		t += delta
 
 func _on_Area_body_entered(body):
-	body.set_mode(RigidBody.MODE_KINEMATIC)
-	
+	if body.get_mode() == RigidBody.MODE_RIGID:
+		body.start_float()	
+
+func _snowball_grabbed(snowball):
+	current_snowball = snowball
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if not event.is_pressed() and current_snowball:
+			current_snowball.release_grab(Vector3())
+			current_snowball = null
